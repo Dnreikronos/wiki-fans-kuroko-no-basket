10dd9df76c2804118e6066373abf02cff8d3c77a
João Roberto Soares <dnreikronos2@gmail.com>
2024-07-28 14:25:08 -0300

    Created some adjustements in the front end of the home page: Setted the background image; Created a title for the page; Created a box for store the characters of kuroko no basket

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import imagem from "../../assets/kuroko-no-basket.jpg";
 
 const HomePage = () => {
   return (
     <>
-    <h1 className='text-3xl font-bold underline'>Welcome to Kuroko no Basket Wiki</h1>
-    </>
-  )
-}
+      <div className="flex flex-col  items-center h-[110vh] bg-background-kuroko">
+        
+        <div className="text-white text-4xl mt-6">
+          <h1>Kuroko no Basket Wiki</h1>
+        </div>
 
-export default HomePage
+        <div className="h-[80vh] w-[150vh] mt-20 rounded-xl bg-white">
+          
+        </div>
+
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
