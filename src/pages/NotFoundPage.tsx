e1fb588241f6bb815e34c09639d5932e07534a01
João Roberto Soares <dnreikronos2@gmail.com>
2024-07-28 16:10:21 -0300

    Changed the font-family

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const NotFoundPage = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center  h-[100vh] bg-background-404 text-5xl text-white font-serif">
+      <div className="flex flex-col items-center justify-center  h-[100vh] bg-background-404 text-5xl text-white font-projectFont">
 
 
         <div className="mb-96"> 
