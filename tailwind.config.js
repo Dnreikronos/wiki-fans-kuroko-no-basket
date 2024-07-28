2b9c535027f319bfe81d7318779371ac5ef21ddd
João Roberto Soares <dnreikronos2@gmail.com>
2024-07-28 15:52:00 -0300

    Features developed in this branch: -Seted a background image for 404 -Maked some adjustements in the layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'background-kuroko': "url('/assets/kuroko-no-basket.jpg')"
+        'background-kuroko': "url('/assets/kuroko-no-basket.jpg')",
+        'background-404': "url('/assets/404-background.png')"
       }
     },
   },
