4365a0b237d8723b5d4136ec25ab9722a545436c
Dnreikronos <dnreikronos2@gmail.com>
2024-08-08 23:10:47 -0300

    Create HomePage with specific placeholder for each character and create specific routes for each character

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -4,25 +4,122 @@ import Placeholder from "react-bootstrap/Placeholder";
 import Button from "react-bootstrap/Button";
 import kagami from "../images/kagami.jpg";
 import tetsuya from "../images/tetsuya.png";
-
+import aomine from "../images/daiki_aomine.png"
+import murasakibara from "../images/murasakibara.png"
+import kise from "../images/kise.png"
+import akashi from "../images/akashi.png"
+import tetsuya2 from "../images/testsuya2.png"
+import midorima from "../images/midorima.png"
 const Characters: React.FC = () => {
   return (
-    
+    <>
+      <div className="h-56 grid grid-cols-4 gap-5 content-start">
       
-      <div className="d-flex justify-content-around bg-slate-50 w-60">
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" className="h-80" src={kagami} />
-          <Card.Body>
-            <Card.Title className="d-flex right-11">Kagami</Card.Title>
-            <Card.Text>
-             
-            </Card.Text>
-            <Button variant="primary">Character Detail</Button>
-          </Card.Body>
-        </Card>
+        <div className="d-flex justify-content-aroun w-70 bg-white rounded-xl">
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" className="h-100" src={kagami} />
+            <Card.Body>
+              <Card.Title className="d-flex text-4xl">Kagami Tsurugi</Card.Title>
+              <Card.Text></Card.Text>
+              <Button variant="primary  text-18rem ">Character Detail</Button>
+            </Card.Body>
+          </Card>
+        </div>
+      
+        <div className="d-flex justify-center  w-70  bg-white rounded-xl">
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" className="h-100" src={tetsuya} />
+            <Card.Body>
+              <Card.Title className="d-flex right-11 text-4xl">
+                Tetsuya Kuroko
+              </Card.Title>
+              <Card.Text></Card.Text>
+              <Button variant="primary  text-18rem ">Character Detail</Button>
+            </Card.Body>
+          </Card>
+        </div>
+
+        <div className="d-flex justify-center  w-70  bg-white rounded-xl">
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" className="h-100" src={aomine} />
+            <Card.Body>
+              <Card.Title className="d-flex right-11 text-4xl">
+                Daiki Aomine
+              </Card.Title>
+              <Card.Text></Card.Text>
+              <Button variant="primary  text-18rem ">Character Detail</Button>
+            </Card.Body>
+          </Card>
+        </div>
+
+
+        <div className="d-flex justify-center  w-70  bg-white rounded-xl">
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" className="h-100" src={murasakibara} />
+            <Card.Body>
+              <Card.Title className="d-flex right-11 text-4xl">
+                Atsushi Murasakibara
+              </Card.Title>
+              <Card.Text></Card.Text>
+              <Button variant="primary  text-18rem ">Character Detail</Button>
+            </Card.Body>
+          </Card>
+        </div>
+
+        <div className="d-flex justify-center  w-70  bg-white rounded-xl">
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" className="h-100" src={midorima} />
+            <Card.Body>
+              <Card.Title className="d-flex right-11 text-4xl">
+                Midorima
+              </Card.Title>
+              <Card.Text></Card.Text>
+              <Button variant="primary  text-18rem ">Character Detail</Button>
+            </Card.Body>
+          </Card>
+        </div>
+
+        <div className="d-flex justify-center  w-70  bg-white rounded-xl">
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" className="h-100" src={akashi} />
+            <Card.Body>
+              <Card.Title className="d-flex right-11 text-4xl">
+               Akashi
+              </Card.Title>
+              <Card.Text></Card.Text>
+              <Button variant="primary  text-18rem ">Character Detail</Button>
+            </Card.Body>
+          </Card>
+        </div>
+
+        <div className="d-flex justify-center  w-70  bg-white rounded-xl">
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" className="h-100" src={kise} />
+            <Card.Body>
+              <Card.Title className="d-flex right-11 text-4xl">
+              Ryōta Kise
+              </Card.Title>
+              <Card.Text></Card.Text>
+              <Button variant="primary  text-18rem ">Character Detail</Button>
+            </Card.Body>
+          </Card>
+        </div>
+
+        <div className="d-flex justify-center  w-70  bg-white rounded-xl">
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" className="h-100" src={tetsuya2} />
+            <Card.Body>
+              <Card.Title className="d-flex right-11 text-4xl">
+                Tetsuya 2#
+              </Card.Title>
+              <Card.Text></Card.Text>
+              <Button variant="primary  text-18rem ">Character Detail</Button>
+            </Card.Body>
+          </Card>
+        </div>
+
       </div>
-    
-    
+    </>
   );
 };
 
