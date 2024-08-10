5a664828155f49e308d900aeac15bfc521f56234
Dnreikronos <dnreikronos2@gmail.com>
2024-08-10 14:51:34 -0300

    Created the galery of characters and did it some adjustementes on the home page css

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -5,7 +5,7 @@ const NotFoundPage = () => {
   return (
     <>
 
-        <div className="flex  felx-col items-center justify-center items-center h-[100vh] bg-backgruond-404 object-center text-5xl text-white font-projectFont ">
+        <div className="flex  felx-col items-center justify-center items-center h-[100vh] bg-backgruond-404 object-center text-5xl text-white font-projectFont">
         
         <div className="mb-96"> 
           <Link to="/"> Go Back Home </Link>
