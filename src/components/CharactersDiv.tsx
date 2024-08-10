0893ed19bbd2daf0f6a38573c8f627c14ace2f1b
Dnreikronos <dnreikronos2@gmail.com>
2024-08-09 23:24:33 -0300

    Implemented materialsUI on project and created a cotainer on charactersDiv

--- src/components/CharactersDiv.tsx
+++ src/components/CharactersDiv.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
 import Placeholder from "react-bootstrap/Placeholder";
-import Button from "react-bootstrap/Button";
+
 import kagami from "../images/kagami.jpg";
 import tetsuya from "../images/tetsuya.png";
 import aomine from "../images/daiki_aomine.png";
@@ -11,12 +11,14 @@ import kise from "../images/kise.png";
 import akashi from "../images/akashi.png";
 import tetsuya2 from "../images/testsuya2.png";
 import midorima from "../images/midorima.png";
+import { Container } from "@mui/material";
+import Button from "@mui/material/Button";
 
 const CharactersDiv = () => {
   return (
     <>
-      <div className="h-56 grid grid-cols-4  gap-5 content-start   bg-opacity-100">
-        
+      <Container maxWidth="xl">
+     
         <div className="d-flex justify-content-aroun w-70  bg-black  rounded-xl">
           <Card style={{ width: "18rem" }}>
             <Card.Img variant="top" className="h-100" src={kagami} />
@@ -25,7 +27,7 @@ const CharactersDiv = () => {
                 Kagami Tsurugi
               </Card.Title>
               <Card.Text></Card.Text>
-              <Button className="text-white" variant="primary  text-18rem ">
+              <Button variant="outlined">
                 <Link to="/DetailKagami">Character Detail</Link>
               </Button>
             </Card.Body>
@@ -40,8 +42,8 @@ const CharactersDiv = () => {
                 Tetsuya Kuroko
               </Card.Title>
               <Card.Text></Card.Text>
-              <Button className="text-white" variant="primary  text-18rem ">
-                <Link to="/DetailKuroko">Character Detail</Link>
+              <Button variant="outlined">
+              <Link to="/DetailKuroko">Character Detail</Link>
               </Button>
             </Card.Body>
           </Card>
@@ -55,8 +57,8 @@ const CharactersDiv = () => {
                 Daiki Aomine
               </Card.Title>
               <Card.Text></Card.Text>
-              <Button className="text-white" variant="primary  text-18rem ">
-                <Link to="/DetailAomine">Character Detail</Link>
+              <Button variant="outlined">
+              <Link to="/DetailAomine">Character Detail</Link>
               </Button>
             </Card.Body>
           </Card>
@@ -70,8 +72,8 @@ const CharactersDiv = () => {
                 Atsushi Murasakibara
               </Card.Title>
               <Card.Text></Card.Text>
-              <Button className="text-white" variant="primary  text-18rem ">
-                <Link to="/DetailMurasakibara">Character Detail</Link>
+              <Button variant="outlined">
+              <Link to="/DetailMurasakibara">Character Detail</Link>
               </Button>
             </Card.Body>
           </Card>
@@ -85,8 +87,8 @@ const CharactersDiv = () => {
                 Midorima
               </Card.Title>
               <Card.Text></Card.Text>
-              <Button className="text-white" variant="primary  text-18rem ">
-                <Link to="/DetailMidorima">Character Detail</Link>
+              <Button variant="outlined">
+              <Link to="/DetailMidorima">Character Detail</Link>
               </Button>
             </Card.Body>
           </Card>
@@ -100,8 +102,8 @@ const CharactersDiv = () => {
                 Akashi
               </Card.Title>
               <Card.Text></Card.Text>
-              <Button className="text-white" variant="primary  text-18rem ">
-                <Link to="/DetailAkashi">Character Detail</Link>
+              <Button variant="outlined">
+              <Link to="/DetailAkashi">Character Detail</Link>
               </Button>
             </Card.Body>
           </Card>
@@ -115,13 +117,14 @@ const CharactersDiv = () => {
                 Ryōta Kise
               </Card.Title>
               <Card.Text></Card.Text>
-              <Button className="text-white" variant="primary  text-18rem ">
-                <Link to="/DetailKise">Character Detail</Link>
+              <Button variant="outlined">
+              <Link to="/DetailKise">Character Detail</Link>
               </Button>
             </Card.Body>
           </Card>
         </div>
-      </div>
+      </Container>
+      {/* </div> */}
     </>
   );
 };
