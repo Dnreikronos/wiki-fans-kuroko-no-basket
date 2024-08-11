3366393bbc43722d4711260b2d5ff40e0776993e
Dnreikronos <dnreikronos2@gmail.com>
2024-08-11 00:56:32 -0300

    feat: Edited the div container on home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,29 +1,23 @@
 import React from "react";
 import imagem from "../../assets/kuroko-no-basket.jpg";
 
-
 import Galery from "../components/Galery";
 
 import { Container } from "@mui/material";
 const HomePage = () => {
-
-  
-
   return (
     <>
       <div className="flex flex-col  font-projectFonts items-center h-screen bg-cover bg-center bg-no-repeat">
-        
-        <div className="bg-gray-50/50 " >
-          <div className="text-white text-center text-4xl mt-6">
-            <h1>Kuroko no Basket Wiki</h1>
-          </div>
-
+        <div className="text-white text-center text-4xl mt-6">
+          <h1>Kuroko no Basket Wiki</h1>
+        </div>
+        <div className="bg-gray-50/50 ">
           <div className="h-[80vh] w-[150vh]   rounded-xl  bg-transparent">
-          <Container maxWidth="xl">
-              <Galery  />
+            <Container maxWidth="xl">
+              <Galery />
             </Container>
           </div>
-          </div>
+        </div>
       </div>
     </>
   );
